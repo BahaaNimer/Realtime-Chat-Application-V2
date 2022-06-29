e583dde32ca990b3e62c1a29d20f03cdc6c6f1b5
BahaaNimer <bahaanimer97@gmail.com>
2022-06-30 02:48:51 +0300

    still need the database

---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+const chatMessages = document.querySelector('.chat-messages');
+const chatForm = document.getElementById('chat-form');
+const roomName = document.getElementById('room-name');
+const usersNames = document.getElementById('users')
+// get username and room name from url 
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+console.log(username, room);
+const socket = io();
+// get room and users
+socket.on('user-rooms-info', ({ room, users }) => {
+  // console.log({room});
+  // let x=users.username
+  // console.log(x);
+  outputRoom(room);
+  outputUsers(users);
+})
+// chatroom joining whth room name and username
+socket.emit('joinRoom', { username, room });
+// message from server
+socket.on('message', message => {
+  // console.log("ratacke from server and handle from index",message);
+  // console.log(message);
+  outputMessage(message);
+  //scroll every comming message
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// submit messsge
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = e.target.message.value;
+  // console.log('sent from index',message);
+  // emmit message to server
+  socket.emit('chatMessage', message);
+  //clear input after send
+  e.target.message.value = '' // focus means empty
+  // focus means empty
+});
+
+// output message to client
+function outputMessage(message) {
+  // console.log("gggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
+  // console.log({message});
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `
+    <p class="meta">${message.username}<span>  ${message.time}</span></p>
+    <p class="text">
+        ${message.text}.
+    </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
+// output room name to client
+function outputRoom(room) {
+  roomName.innerText = room;
+}
+// output users to client
+function outputUsers(users) {
+  console.log({ users });
+  usersNames.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`
+}
---
+++ server.js
@@ -0,0 +1,80 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const PORT = process.env.PORT;
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const formateMessage = require('./utils/messages');
+const {
+  join,
+  getUser,
+  userLeave,
+  getRoom,
+  getall
+} = require('./utils/users');
+// console.log({join});
+// set static folder
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(express.static(path.join(__dirname, 'public')));
+// run when client connect
+io.on('connection', socket => {
+  // console.log("doooooo");
+  // handle join from client with name of room and username
+  socket.on('joinRoom', ({
+    username,
+    room
+  }) => {
+    // console.log(username, room);
+    const user = join(socket.id, username, room);
+    socket.join(user.room)
+    // send welcome message when join chat
+    console.log('allllllllllllllllll', getall());
+    socket.emit('message', formateMessage('LTUC BOT', 'welcome to LTUC chat'));
+    //WHEN USER CONNECT
+    socket.broadcast.to(user.room).emit('message', formateMessage('LTUC BOT', `${user.username} has joined chat`));
+    // send users and room info
+    // let x=user.room;
+    // console.log('sssssssss',{x});
+    // let xx=getRoom(user.room);
+    // console.log({xx});
+    io.to(user.room).emit('user-rooms-info', ({
+      room: user.room,
+      users: getRoom(user.room)
+    }))
+  });
+  //handle when client leave chat
+  socket.on('disconnect', () => {
+    // to know wich user leave
+    const user = userLeave(socket.id);
+    // console.log({
+    //     user
+    // });
+    // check if there user
+    if (user) {
+      //this send to all users
+      io.to(user[0].room).emit('message', formateMessage('LTUC BOT', `${user[0].username} has left the chat`));
+      // send users and room info
+      io.to(user.room).emit('user-rooms-info', ({
+        room: user[0].room,
+        users: getRoom()
+      }))
+    }
+  });
+  // catch chatMessage from client
+  socket.on('chatMessage', message => {
+    // console.log('sent from index and handle from server', message);
+    console.log(socket.id);
+    const user = getUser(socket.id);
+    // console.log('aaaaaaaaaaaaaaaaaaaaa',user);
+    let x = user[0].username;
+    // console.log({x});
+    io.to(user[0].room).emit('message', formateMessage(user[0].username, message));
+  });
+})
+// console.log(PORT);
+server.listen(PORT, () => {
+  console.log(`server lestining on port ${PORT}`);
+});
